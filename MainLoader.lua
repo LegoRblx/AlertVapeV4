6f011007d6daf9f9d46f84abd89da204408f8fec
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-09-07 15:48:09 -0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -104,7 +104,7 @@ local CustomFly = COB("Blatant", {
 					game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Climbing)
 					task.wait(0.01)
 					game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Landed)
-					task.wait(0.02)
+					task.wait(0.1)
 					game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.FreeFalling)
 				end
 			end)
