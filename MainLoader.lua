e7ccea56fc3218b7407d728fd191f4ed683d838b
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-09-07 17:42:40 -0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until shared.GuiLibrary
-repeat task.wait() until shared.VapeExecuted and shared.VapeFullyLoaded
+repeat task.wait() until shared.VapeExecuted
 local a=syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()end
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(b)
 		if b==Enum.TeleportState.Started then a("pcall(function() shared.AlertLoaded = false end)") end end)
