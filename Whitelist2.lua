ea761f8e166eafa02223c992657031e591deeaaa
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-09-10 14:50:16 -0700

    Update Whitelist2.lua

--- Whitelist2.lua
+++ Whitelist2.lua
@@ -1,5 +1,22 @@
 {
   "players": {},
-   "owners": {},
-  "chattags": {},
+   "owners": {"82a8db09961c7009f04660052eb9cb511d5f77d75531fc99ee49a60130721b45a0d43ee2f74050ef6fee69a9eed5d6cc7cc2e1a5171546d16dbbab6e91a35f8f"},
+  "chattags": {
+    "f4fe1a525f72679c55fd3772fdad8c678404fc554fab96589b1ba715cd8bbd6a1877706364e24dffd696d3bd0f3da3973550d9a02eda8d64dba75a1fe6265e5b": {
+      "NameColor": {
+        "r": 200,
+        "g": 80,
+        "b": 255
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 200,
+            "g": 80,
+            "b": 255
+          },
+          "TagText": "VAPE OWNER"
+        }
+      ]
+    },},
   }
